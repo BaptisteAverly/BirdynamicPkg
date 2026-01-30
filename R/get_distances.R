8f44f59c8275521e74efc89f7deed285486011e0
Baptiste Averly <baptiste.averly@gmail.com>
2026-01-30 11:48:55 +0100

    created function file for get_distances

---
+++ R/get_distances.R
@@ -0,0 +1,16 @@
+
+#' Calcul des distances entre parc éoliens et colonies
+#'
+#' @param colonies tableau de colonies
+#' @param parcs shapefile des parcs
+#' @param costMatrix matrice de couts
+#' @param doShpa booleen
+#' @param progress uniquement pour une utilisation avec shiny
+#'
+#' @returns matrice de distance parc-colonies
+#' @export
+#'
+get_distances <- function(colonies,parcs,costMatrix,doShpa=T,progress=NULL){
+
+
+}
