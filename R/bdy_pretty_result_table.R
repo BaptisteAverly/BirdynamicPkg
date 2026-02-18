f0c67de6c8332f4992f76a8577615d32a547abf7
Victor Cazalis <victor.cazalis@conservara.fr>
2026-02-18 11:39:45 +0100

    Update percentages in outputs

--- R/bdy_pretty_result_table.R
+++ R/bdy_pretty_result_table.R
@@ -23,8 +23,8 @@ bdy_pretty_result_table <- function(Raw_ResTables, type){
     mutate(
       Population_initiale = paste0(round(Pop_Init_med), " [", round(Pop_Init_2.5), "-", round(Pop_Init_97.5), "]"),
       Nombre_collisions = paste0(round(Mortality_med, 2), " [", round(Mortality_2.5, 2), "-", round(Mortality_97.5, 2), "]"),
-      Impact_Relatif = paste0(round(RelImpact_med, 2), " [", round(RelImpact_2.5, 2), "-", round(RelImpact_97.5, 2), "]"),
-      Augmentation_Extinction=round(Ext_Relative,2)
+      Impact_Relatif = paste0(round(RelImpact_med, 2), "% [", round(RelImpact_2.5, 2), "-", round(RelImpact_97.5, 2), "]"),
+      Augmentation_Extinction=paste0(round(Ext_Relative,2), "%")
     ) %>%
     dplyr::rename(Espece=Species) %>%
     subset(., select=names(.)[grepl("_med", names(.))==F & grepl("_2.5", names(.))==F & grepl("_97.5", names(.))==F & grepl("Ext_", names(.))==F])
@@ -32,9 +32,9 @@ bdy_pretty_result_table <- function(Raw_ResTables, type){
   # Add max impact
   if(type=="national"){
     ResTable_MaxIR <- Raw_ResTables$Tableau_Subpop %>% group_by(Species) %>%  slice(which.max(RelImpact_med))
-    ResTable_MaxIR$Text <- ifelse(ResTable_MaxIR$RelImpact_med==0, 0, paste0(round(ResTable_MaxIR$RelImpact_med,4), " (Groupe de colonies : ", ResTable_MaxIR$GrColo, ")"))
+    ResTable_MaxIR$Text <- ifelse(ResTable_MaxIR$RelImpact_med==0, 0, paste0(round(ResTable_MaxIR$RelImpact_med,4), "% (Groupe de colonies : ", ResTable_MaxIR$GrColo, ")"))
     ResTable_MaxExt <- Raw_ResTables$Tableau_Subpop %>% group_by(Species) %>%  slice(which.max(Ext_Relative))
-    ResTable_MaxExt$Text <- ifelse(ResTable_MaxExt$Ext_Relative==0, 0, paste0(round(ResTable_MaxExt$Ext_Relative,4), " (Groupe de colonies : ", ResTable_MaxExt$GrColo, ")"))
+    ResTable_MaxExt$Text <- ifelse(ResTable_MaxExt$Ext_Relative==0, 0, paste0(round(ResTable_MaxExt$Ext_Relative,4), "% (Groupe de colonies : ", ResTable_MaxExt$GrColo, ")"))
     ResTable$MAX_Impact_relatif <- ResTable_MaxIR$Text[match(ResTable$Espece, ResTable_MaxIR$Species)]
     ResTable$MAX_Augmentation_extinction <- ResTable_MaxExt$Text[match(ResTable$Espece, ResTable_MaxExt$Species)]
   }
