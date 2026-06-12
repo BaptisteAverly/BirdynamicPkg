4c1b290746862852d316ea1e603e1af326b2f656
Victor Cazalis <victor.cazalis@conservara.fr>
2026-06-12 09:10:58 +0200

    Tempo fix for bdy_plot_windfarm_impact

--- R/bdy_plot_windfarm_impact.R
+++ R/bdy_plot_windfarm_impact.R
@@ -12,12 +12,12 @@
 bdy_plot_windfarm_impact <- function(Raw_ResTables, mortalities){
 
   moyenne <- mortalities %>%
-    dplyr::group_by(espece_latin, parc) %>%
+    dplyr::group_by(espece_latin, espece, parc) %>%
     dplyr::summarise(Mean=mean(coefficient, na.rm=T)) %>%
     group_by(espece_latin) %>%
     mutate(Prop_morta = Mean/sum(Mean,na.rm=T))
 
-  moyenne$IR <- Raw_ResTables$Tableau_National$RelImpact_med[match(moyenne$espece_latin, Raw_ResTables$Tableau_National$Species)]
+  moyenne$IR <- Raw_ResTables$Tableau_National$RelImpact_med[match(moyenne$espece, Raw_ResTables$Tableau_National$Species)]
   moyenne$Impact_parc <- moyenne$IR * moyenne$Prop_morta
 
   G <- ggplot(moyenne)+
